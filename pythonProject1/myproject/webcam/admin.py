1246a05259dbffbeaea0be8b26aa5d6aba41db2d
Mateusz Powirski <92019610+P0wir@users.noreply.github.com>
2024-01-04 04:49:13 +0100

    Delete pythonProject1 directory

--- pythonProject1/myproject/webcam/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from webcam import models
-
-admin.site.register(models.Category)
-admin.site.register(models.Webcam)
-admin.site.register(models.Comment)
--- pythonProject1/myproject/webcam/helpers.py
+++
@@ -1,4 +0,0 @@
-from collections import namedtuple
-from math import radians, sin, cos, sqrt, atan2
-
-Location = namedtuple('Location', ['latitude', 'longitude'])
